5cd27073c4c5bb0b51c206e2ee004e9a1e7fb22c
peterjcole <11048765+peterjcole@users.noreply.github.com>
2020-08-06 15:43:21 +0100

    end of lesson 8

--- src/App.vue
+++ src/App.vue
@@ -1,103 +1,22 @@
 <template>
   <div>
     <div class="nav-bar"></div>
-    <div class="product">
-      <div class="product-image">
-        <img :src="image" />
-      </div>
-      <div class="product-info">
-        <a :href="link">
-          <h1>{{ title }}</h1>
-        </a>
-        <p v-if="inStock > 10">In Stock</p>
-        <p v-else-if="inStock <= 10 && inStock > 0">Selling fast!</p>
-        <p v-else :class="{ lineThrough: true }">Out of Stock</p>
-        <span v-show="onSale">On sale!</span>
-        <p>Details:</p>
-        <ul>
-          <li v-for="(detail, index) in details" :key="index">{{ detail }}</li>
-        </ul>
-        <p>Sizes:</p>
-        <ul>
-          <li v-for="(size, index) in sizes" :key="index">{{ size }}</li>
-        </ul>
-        <p>Colours:</p>
-        <div v-for="(variant, index) in variants" 
-          :key="variant.variantId"
-          class="color-box"
-          :style="{ backgroundColor: variant.variantColor }"
-          @mouseover="updateProduct(index)">
-        </div>
-        <p>
-          <button v-on:click="addToCart" 
-            :disabled="!inStock"
-            :class="{ disabledButton: !inStock }">Add to cart</button>
-        </p>
-        <p>
-          <button v-on:click="removeFromCart">Remove from cart</button>
-        </p>
-        <div class="cart">
-          <p>Cart ({{ cart }})</p>
-        </div>
-      </div>
-    </div>
+    <Product :premium="premium" />
   </div>
 </template>
 
 <script>
+import Product from './components/Product'
+
 export default {
   name: "App",
   data: () => {
     return {
-      product: "Socks",
-      brand: "Unknown Branded",
-      selectedVariant: 0,
-      link: "http://teeturtle.com",
-      details: ["Amazing happy octopus", "BPA free"],
-      variants: [
-        {
-          variantId: 1234,
-          variantColor: "powderblue",
-          variantImage: require("./assets/blue.jpg"),
-          variantQuantity: 0,
-          variantOnSale: true
-        },
-        {
-          variantId: 1235,
-          variantColor: "mediumturquoise",
-          variantImage: require("./assets/bluer.jpg"),
-          variantQuantity: 10,
-          variantOnSale: false
-        },
-      ],
-      sizes: ["8", "9", "10"],
-      cart: 0,
+      premium: false
     }
   },
-  methods: {
-    addToCart() {
-      this.cart += 1
-    },
-    removeFromCart() {
-      this.cart -= this.cart && 1 
-    },
-    updateProduct(index) {
-      this.selectedVariant = index
-    },
-  },
-  computed: {
-    title() {
-      return `${this.brand} ${this.product}`
-    },
-    image() {
-      return this.variants[this.selectedVariant].variantImage
-    },
-    inStock() {
-      return this.variants[this.selectedVariant].variantQuantity
-    },
-    onSale() {
-      return this.variants[this.selectedVariant].variantOnSale
-    }
+  components: {
+    Product
   }
 }
 </script>
---
+++ src/components/Details.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <p>Details:</p>
+  <ul>
+    <li v-for="(detail, index) in details" :key="index">{{ detail }}</li>
+  </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    details: {
+      type: [String],
+      required: true
+    }
+  }
+}
+</script>
